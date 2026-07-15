8a92cb595808534b570fa039a0ccd8e92a0795f5
SeradedStripes <seradedstripes@gmail.com>
2026-07-16 01:00:39 +0200

    User data exports migration

---
+++ db/migrate/20260715225859_create_user_data_exports.rb
@@ -0,0 +1,14 @@
+class CreateUserDataExports < ActiveRecord::Migration[8.1]
+  def change
+    create_table :user_data_exports do |t|
+      t.references :user, null: false, foreign_key: true
+      t.string :status, null: false, default: "pending"
+      t.text :error_message
+      t.string :zip_filename
+
+      t.timestamps
+    end
+
+    add_index :user_data_exports, [:user_id, :status]
+  end
+end
