555d25c83fd0cad1e584e3150e60b7101589ab06
SeradedStripes <seradedstripes@gmail.com>
2026-07-16 04:34:55 +0200

    Update MyPolicy with data export authorization

--- app/policies/my_policy.rb
+++ app/policies/my_policy.rb
@@ -30,4 +30,16 @@ class MyPolicy < ApplicationPolicy
   def update_notification_settings?
     signed_in_any?
   end
+
+  def show_data_exports?
+    signed_in_any?
+  end
+
+  def create_data_export?
+    signed_in_any?
+  end
+
+  def download_data_export?
+    signed_in_any?
+  end
 end
